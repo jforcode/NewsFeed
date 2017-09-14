577d47e6c547fb5825d72ef492d2251bea4786f7
jforcode <jforcode@gmail.com>
2017-09-14 16:49:53 +0530

    initial commit

---
+++ app/src/main/java/com/jeevan/inshorts/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jeevan.inshorts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+}
